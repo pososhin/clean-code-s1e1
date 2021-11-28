48225cd86b4138e7a49ceb22476dd65a7dc4367e
pososhin <pososhin@gmail.com>
2021-11-28 05:43:30 +0300

    refactor: Tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("main__todo");//ul of #main__todo
-var completedTasksHolder=document.getElementById("main__completed");//main__completed
+var incompleteTaskHolder=document.getElementById("main__todo-list");//ul of #main__todo-list
+var completedTasksHolder=document.getElementById("main__completed-list");//main__completed-list
 
 
 //New task list item
@@ -33,21 +33,23 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
-
+    label.className='task main__todo-label';
+    
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="main__todo-input main__todo-input-checkbox";
     editInput.type="text";
-    editInput.className="task";
-
+    editInput.className="task main__todo-input task__input";
+    
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="button-edit";
-
-    deleteButton.className="button-delete";
+    
+    deleteButton.className="button button-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='button-delete__img';
     deleteButton.appendChild(deleteButtonImg);
 
-
+    listItem.className="main__todo";
     //and appending.
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
@@ -117,7 +119,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
     console.log("Complete Task...");
 
-    //Append the task list item to the #main__completed
+    //Append the task list item to the #main__completed-list
     var listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
@@ -129,7 +131,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #main__todo.
+    //Append the task list item to the #main__todo-list.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
@@ -148,6 +150,7 @@ var ajaxRequest=function(){
 addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
+addButton.classList.add("button-add");
 
 
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
